5edca8e88e19cd8a3d0a158975390c23cc05eecf
emre <emreozturanli1@gmail.com>
2022-07-10 21:43:33 +0300

    LETTER CASE

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ const Project = ({id,image,name,link,tool,source}) => {
   return (
     <Unit>
         <h3>{name}</h3>
-        <img src={`./images/${image}.png`} alt={name} />
+        <img src={`./images/${image}.PNG`} alt={name} />
         <a href={source} target="_blank" rel="noopener noreferrer" >Source Code</a> 
         <a href={link} target="_blank" rel="noopener noreferrer">Visit Page</a>
     </Unit>
